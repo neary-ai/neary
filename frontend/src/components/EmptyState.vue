7ffb1c8e2465b4d684618eb985a8837f5311d34b
mightworks <joe@might.works>
2023-08-04 21:34:07 -0600

    add responsive new conversation state

--- frontend/src/components/EmptyState.vue
+++ frontend/src/components/EmptyState.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="w-full h-screen flex flex-col items-center justify-start mt-12 px-8 pb-6">
+  <div :class="[isTall ? 'justify-start' : 'justify-center', 'w-full h-screen flex flex-col items-center mt-12 px-8 pb-6']">
     <Icon icon="uil:tornado" class="w-14 h-14 text-nearypink-300/70 mb-6" />
     <div class="text-xl text-slate-300 font-semibold mb-2 text-center">Ready? Just start typing.</div>
-    <div class="w-full max-w-sm mt-8">
+    <div v-if="isTall" class="w-full max-w-sm mt-8">
       <div class="flex flex-col gap-2.5">
         <label class="text-sm text-field-label">Choose a title</label>
         <input type="text" placeholder="Enter your title" v-model="conversationTitle" @blur="saveConversationSettings"
@@ -17,20 +17,39 @@
         <ProgramCard />
       </div>
     </div>
+    <div v-else class="w-full max-w-xs">
+      <div @click="toggleSettings()" class="cursor-pointer flex items-center justify-center text-nearygray-400 text-sm mt-3">Conversation settings &rarr;</div>
+    </div>
   </div>
 </template>
 
 <script setup>
-import { ref, computed, watch, } from 'vue';
+import { ref, computed, watch, onUnmounted } from 'vue';
+import { useRouter, useRoute } from 'vue-router';
 import { Icon } from '@iconify/vue';
 import ListBoxBasic from './common/ListBoxBasic.vue'
 import { useAppStore } from '@/store/index.js';
 import ProgramCard from './ProgramCard.vue';
 
 const store = useAppStore();
+const route = useRoute();
+const router = useRouter();
 
 const conversationTitle = ref(null);
 const selectedSpaceValue = ref(null);
+const windowHeight = ref(window.innerHeight);
+
+const updateHeight = () => {
+  windowHeight.value = window.innerHeight;
+};
+
+window.addEventListener('resize', updateHeight);
+
+onUnmounted(() => {
+  window.removeEventListener('resize', updateHeight);
+});
+
+const isTall = computed(() => windowHeight.value > 680);
 
 const selectedSpace = computed(() => {
   if (store.conversationSettings.space && store.conversationSettings.space.value) {
@@ -47,6 +66,14 @@ const saveConversationSettings = async () => {
   let settings = { 'title': conversationTitle.value, 'space': selectedSpaceValue.value }
   await store.updateConversationSettings(store.selectedConversationId, settings);
 };
+
+const toggleSettings = () => {
+  if (route.path.startsWith('/settings')) {
+    router.push('/');
+  } else {
+    router.push(`/settings/${store.selectedConversationId}`);
+  }
+}
 
 watch(
   () => store.conversationSettings,
