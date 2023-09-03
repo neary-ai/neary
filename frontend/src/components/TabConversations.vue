8a49b9db632e5096685d17cd6c6e0d69af01e976
mightworks <joe@might.works>
2023-09-03 17:08:43 -0600

    add icons in mobile nav

--- frontend/src/components/TabConversations.vue
+++ frontend/src/components/TabConversations.vue
@@ -77,11 +77,9 @@
           class="mb-2 mt-4 px-3 text-xs font-semibold text-field-default-foreground">All Conversations</h2>
         <ul class="divide-y divide-field-divide">
           <li v-for="conversation in filteredConversations.nonSpaceConversations" :key="conversation.id"
-            class="group flex cursor-pointer select-none items-center pl-2 pr-4 py-2.5 hover:bg-field-active hover:text-field-active-foreground"
+            class="group flex cursor-pointer select-none items-start pl-2 pr-4 py-2.5 hover:bg-field-active hover:text-field-active-foreground"
             @click="store.loadConversation(conversation.id, close)">
-            <div>
-              <Icon :icon="conversation.preset.icon ? conversation.preset.icon : 'heroicons:user-solid'" class="h-5 w-5 mr-3 ml-2 opacity-70" />
-            </div>
+            <Icon :icon="conversation.preset.icon ? conversation.preset.icon : 'heroicons:user-solid'" class="mt-0.5 h-5 w-5 mr-2 opacity-70" />
             <div class="flex flex-col items-start w-80 pr-2">
               <div class="font-medium truncate w-full pb-0.5">{{
                 conversation.title
--- frontend/src/components/common/Modal.vue
+++ frontend/src/components/common/Modal.vue
@@ -6,7 +6,7 @@
                 <div class="fixed inset-0 bg-nearyblue-300 bg-opacity-80 transition-opacity" />
             </TransitionChild>
             <div class="fixed inset-0 z-10 overflow-y-auto">
-                <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                <div class="flex min-h-full items-center justify-center p-4 text-center sm:p-0">
                     <TransitionChild as="template" enter="ease-out duration-300"
                         enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                         enter-to="opacity-100 translate-y-0 sm:scale-100" leave="ease-in duration-200"
